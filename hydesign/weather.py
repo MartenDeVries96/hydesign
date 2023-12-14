0ffb9c64113c478d49328a8bb75c8d16ac15b81d
jumu <jumu@dtu.dk>
2023-12-14 15:05:26 +0100

    existing wind component uses wind farm wake model results

--- hydesign/weather.py
+++ hydesign/weather.py
@@ -68,6 +68,54 @@ class ABL(om.ExplicitComponent):
         partials['wst', 'hh'] = ds_interpolated.dWS_dz.values.flatten()
 
 
+class ABL_WD(om.ExplicitComponent):
+    """Atmospheric boundary layer WS and WD interpolation
+    
+    Parameters
+    ----------
+    hh : Turbine's hub height
+
+    Returns
+    -------
+    wst : wind speed time series at the hub height
+
+    """
+
+
+    def __init__(self, weather_fn, N_time):
+        super().__init__()
+        self.weather_fn = weather_fn
+        self.N_time = N_time
+
+    def setup(self):
+        self.add_input('hh',
+                       desc="Turbine's hub height",
+                       units='m')
+        self.add_output('wst',
+                        desc="ws time series at the hub height",
+                        units='m/s',
+                        shape=[self.N_time])
+        self.add_output('wdt',
+                        desc="wd time series at the hub height",
+                        units='deg',
+                        shape=[self.N_time])
+
+    def precompute(self, hh):
+
+        weather = pd.read_csv(self.weather_fn, index_col=0, parse_dates=True)
+        ds_interpolated = interpolate_WS_loglog(weather, hh=hh)
+        ds_interpolated_aux = interpolate_WD_linear(weather, hh=hh)
+        ds_interpolated['WD'] = ds_interpolated_aux['WD']
+        return ds_interpolated
+
+    def compute(self, inputs, outputs):
+
+        ds_interpolated = self.precompute(inputs['hh'])
+        self.ds_interpolated = ds_interpolated
+
+        outputs['wst'] = ds_interpolated.WS.values.flatten()
+        outputs['wdt'] = ds_interpolated.WD.values.flatten()
+
 # -----------------------------------------------------------------------
 # Auxiliar functions for weather handling
 # -----------------------------------------------------------------------
@@ -118,10 +166,56 @@ def interpolate_WS_loglog(weather, hh):
     return ds_interpolated
 
 
+def interpolate_WD_linear(weather, hh):
+    """
+    Auxiliar functions for WD interpolation.
+
+    Parameters
+    ----------
+    weather: pd.DataFrame
+        WD time-series table for a location at multiple heights.
+        The columns must be named WS_hh (for example  WD_10, WD_50, ...).
+    
+    hh: float
+        Elevation (of a wind turbine) to interpolate WD
+
+    Returns
+    --------
+    ds_interpolated: xr.Dataset
+        Dataset that contains the interpolated time-series: WD and dWD_dz
+
+    """
+    wd_vars = [var for var in weather.columns if 'WD_' in var]
+    heights = np.array([float(var.split('_')[-1]) for var in wd_vars])
+
+
+    ds_all = xr.Dataset(
+        data_vars = {
+            'WD': (['time','height'], weather[wd_vars].values),
+            },
+        coords = {
+            'time': weather.index.values,
+            'height': heights,
+            }  
+        )
+
+    ds_all['WD_x'] = cosd(ds_all.WD)
+    ds_all['WD_y'] = sind(ds_all.WD)
+    
+    ds_interpolated = xr.Dataset()
+    ds_interpolated['WD_x'] = ds_all.WD_x.interp(height=hh, method='linear')
+    ds_interpolated['WD_y'] = ds_all.WD_x.interp(height=hh, method='linear')
+    
+    ds_interpolated['WD'] = np.mod( np.arctan2(ds_interpolated['WD_y'],ds_interpolated['WD_x'])*360/(2*np.pi) , 360)
+    
+    return ds_interpolated
+
 # -----------------------------------------------------------------
 # Auxiliar functions for extractions on a ERA5 database
 # -----------------------------------------------------------------
 
+cosd = lambda x: np.cos(np.degrees(x))
+sind = lambda x: np.sin(np.degrees(x))
 
 # pvlib imports
 from pvlib import pvsystem, tools, irradiance, atmosphere
@@ -606,14 +700,34 @@ def apply_interpolation_f(
 
     # linear profile in z
     for var in vars_xyz:
-        interp[var] = (wrf_ds.get(var).isel({
-            var_x_grid: weights_ds.ind_x,
-            var_y_grid: weights_ds.ind_y,
-            var_z_grid: weights_ds.ind_z,
-        })
-            * weights_ds.weights_x
-            * weights_ds.weights_y
-            * weights_ds.weights_z).sum(['ix', 'iy', 'iz'])
+        if var != varWD:
+            interp[var] = (wrf_ds.get(var).isel({
+                var_x_grid: weights_ds.ind_x,
+                var_y_grid: weights_ds.ind_y,
+                var_z_grid: weights_ds.ind_z,
+            })
+                * weights_ds.weights_x
+                * weights_ds.weights_y
+                * weights_ds.weights_z).sum(['ix', 'iy', 'iz'])
+        else:
+            # Apply interpolation in WD using vector linear interpolation 
+            interp[varWD+'_x'] = (cosd(wrf_ds.get(var).isel({
+                var_x_grid: weights_ds.ind_x,
+                var_y_grid: weights_ds.ind_y,
+                var_z_grid: weights_ds.ind_z,
+            }))
+                * weights_ds.weights_x
+                * weights_ds.weights_y
+                * weights_ds.weights_z).sum(['ix', 'iy', 'iz'])
+
+            interp[varWD+'_y'] = (sind(wrf_ds.get(var).isel({
+                var_x_grid: weights_ds.ind_x,
+                var_y_grid: weights_ds.ind_y,
+                var_z_grid: weights_ds.ind_z,
+            }))
+                * weights_ds.weights_x
+                * weights_ds.weights_y
+                * weights_ds.weights_z).sum(['ix', 'iy', 'iz'])
 
     # only horizontal interpolation
     for var in vars_xy:
@@ -641,7 +755,7 @@ def apply_interpolation_f(
 
     if varWD in vars_xy_logz + vars_xyz + \
             vars_xy + vars_nearest_xy + vars_nearest_xyz:
-        interp[varWD] = np.mod(interp[varWD], 360)
+        interp[varWD] = np.mod( np.arctan2(interp[varWD+'_y'],interp[varWD+'_x'])*360/(2*np.pi) , 360)
 
     return interp
 
--- hydesign/wind.py
+++ hydesign/wind.py
@@ -341,6 +341,245 @@ class wpp_with_degradation(om.ExplicitComponent):
             life_h = self.life_h, 
             share = self.share_WT_deg_types)
 
+class existing_wpp(om.ExplicitComponent):
+    """
+    Wind power plant model for an existing layout
+
+    Provides the wind power time series using wake affected power curve and the wind speed time series.
+
+    Parameters
+    ----------
+    N_time : Number of time-steps in weather simulation
+    existing_wpp_power_curve_xr_fn: File name of a netcdf xarray. 
+            
+            The xarray should include 'P_no_wake' as function of 'ws' and 'wake_losses' as a function of 'ws' and 'wd'.
+            Note that the wd must include both 0 and 360, and a large WS (for interpolation). 
+            Resolution of ws and wd is flexible.
+            
+            '''
+            <xarray.Dataset>
+            Dimensions:          (ws: 53, wd: 361)
+            Coordinates:
+              * ws               (ws) float64 0.0 0.5 1.0 1.5 2.0 ... 24.5 25.0 25.0 100.0
+              * wd               (wd) float64 0.0 1.0 2.0 3.0 ... 357.0 358.0 359.0 360.0
+            Data variables:
+                wake_losses_eff  (ws, wd) float64 0.0 0.0 0.0 0.0 0.0 ... 0.0 0.0 0.0 0.0
+                P_no_wake        (ws) float64 0.0 0.0 0.0 0.0 0.0 ... 100.0 100.0 0.0 0.0
+            '''
+            
+    wpp_efficiency : WPP efficiency
+    wst : wind speed time series at the hub height
+    wdt : wind direction time series at the hub height
+
+    Returns
+    -------
+    wind_t_ext_deg : power time series with degradation extended through lifetime
+
+    """
+
+    def __init__(
+        self, 
+        N_time,
+        existing_wpp_power_curve_xr_fn, 
+        wpp_efficiency = 0.95,
+        ):
+        
+        super().__init__()
+        self.N_time = N_time
+        self.wpp_efficiency = wpp_efficiency
+
+        self.existing_wpp_power_curve_xr_fn = existing_wpp_power_curve_xr_fn
+        
+        
+    def setup(self):
+        self.add_input('wst',
+                       desc="ws time series at the hub height",
+                       units='m/s',
+                       shape=[self.N_time])
+        self.add_input('wdt',
+                       desc="wd time series at the hub height",
+                       units='deg',
+                       shape=[self.N_time])
+
+        self.add_output('wind_t',
+                        desc="power time series at the hub height",
+                        units='MW',
+                        shape=[self.N_time])
+
+
+    def compute(self, inputs, outputs):
+
+        N_time = self.N_time
+        life_h = self.life_h
+        wpp_efficiency = self.wpp_efficiency
+        
+        wst = inputs['wst']
+        wdt = inputs['wst']
+
+        existing_wpp_power_curve_xr = xr.open_dataset(self.existing_wpp_power_curve_xr_fn)
+        
+        xr_time = xr.Dataset()
+        xr_time['wst'] = xr.DataArray( 
+            data=wst, 
+            dims = ['t'],
+            coords = {'t':np.arange(N_time)})
+        xr_time['wdt'] = xr.DataArray( 
+            data=wdt, 
+            dims = ['t'],
+            coords = {'t':np.arange(N_time)})
+
+        wake_loss_t = existing_wpp_power_curve_xr.wake_losses.interp(ws=xr_time.wst, wd=xr_time.wdt).values
+
+        wind_t_ext_deg_no_wake = get_wind_ts(
+            ws = existing_wpp_power_curve_xr.ws.values,
+            pcw = existing_wpp_power_curve_xr.P_no_wake.values,
+            wst = wst,
+            wpp_efficiency = self.wpp_efficiency,
+        )
+        
+        outputs['wind_t'] = wake_loss_t_ext*wind_t_ext_deg_no_wake
+        
+        outputs['wind_t_ext_deg'] = wind_t_ext_deg
+
+class existing_wpp_with_degradation(om.ExplicitComponent):
+    """
+    Wind power plant model for an existing layout
+
+    Provides the wind power time series using wake affected power curve and the wind speed time series.
+
+    Parameters
+    ----------
+    N_time : Number of time-steps in weather simulation
+    life_h : lifetime in hours
+    existing_wpp_power_curve_xr_fn: File name of a netcdf xarray. 
+            
+            The xarray should include 'P_no_wake' as function of 'ws' and 'wake_losses' as a function of 'ws' and 'wd'.
+            Note that the wd must include both 0 and 360, and a large WS (for interpolation). 
+            Resolution of ws and wd is flexible.
+            
+            '''
+            <xarray.Dataset>
+            Dimensions:          (ws: 53, wd: 361)
+            Coordinates:
+              * ws               (ws) float64 0.0 0.5 1.0 1.5 2.0 ... 24.5 25.0 25.0 100.0
+              * wd               (wd) float64 0.0 1.0 2.0 3.0 ... 357.0 358.0 359.0 360.0
+            Data variables:
+                wake_losses_eff  (ws, wd) float64 0.0 0.0 0.0 0.0 0.0 ... 0.0 0.0 0.0 0.0
+                P_no_wake        (ws) float64 0.0 0.0 0.0 0.0 0.0 ... 100.0 100.0 0.0 0.0           
+            '''
+            
+    wpp_efficiency : WPP efficiency
+    wind_deg_yr : year list for providing WT degradation curve
+    wind_deg : degradation losses at yr
+    share_WT_deg_types : share ratio between two degradation mechanism (0: only shift in power curve, 1: degradation as a loss factor )
+    ws : Power curve wind speed list
+    pcw : Wake affected power curve
+    wst : wind speed time series at the hub height
+
+    Returns
+    -------
+    wind_t_ext_deg : power time series with degradation extended through lifetime
+
+    """
+
+    def __init__(
+        self, 
+        N_time,
+        existing_wpp_power_curve_xr_fn, 
+        wpp_efficiency = 0.95,
+        life_h = 25*365*24,
+        wind_deg_yr = [0, 25],
+        wind_deg = [0, 25*1/100],
+        share_WT_deg_types = 0.5,
+        weeks_per_season_per_year = None,
+        ):
+        
+        super().__init__()
+        self.N_time = N_time
+        self.life_h = life_h
+        self.wpp_efficiency = wpp_efficiency
+
+        self.existing_wpp_power_curve_xr_fn = existing_wpp_power_curve_xr_fn
+        
+        # number of elements in WT degradation curve
+        self.wind_deg_yr = wind_deg_yr
+        self.wind_deg = wind_deg
+        self.share_WT_deg_types = share_WT_deg_types
+
+        # In case data is provided as weeks per season
+        self.weeks_per_season_per_year = weeks_per_season_per_year
+        
+    def setup(self):
+        self.add_input('wst',
+                       desc="ws time series at the hub height",
+                       units='m/s',
+                       shape=[self.N_time])
+        self.add_input('wdt',
+                       desc="wd time series at the hub height",
+                       units='deg',
+                       shape=[self.N_time])
+
+        self.add_output('wind_t_ext_deg',
+                        desc="power time series with degradation",
+                        units='MW',
+                        shape=[self.life_h])
+
+
+    def compute(self, inputs, outputs):
+
+        N_time = self.N_time
+        life_h = self.life_h
+        wpp_efficiency = self.wpp_efficiency
+
+        existing_wpp_power_curve_xr = xr.open_dataset(self.existing_wpp_power_curve_xr_fn)
+        
+        # number of elements in WT degradation curve
+        wind_deg_yr = self.wind_deg_yr
+        wind_deg = self.wind_deg
+        share_WT_deg_types = self.share_WT_deg_types
+
+        # In case data is provided as weeks per season
+        weeks_per_season_per_year = self.weeks_per_season_per_year
+        
+        wst = inputs['wst']
+        wst_ext = expand_to_lifetime(
+            wst, life_h = life_h, weeks_per_season_per_year = weeks_per_season_per_year)
+
+        wdt = inputs['wdt']
+        wdt_ext = expand_to_lifetime(
+            wdt, life_h = life_h, weeks_per_season_per_year = weeks_per_season_per_year)
+
+        xr_time = xr.Dataset()
+        xr_time['wst'] = xr.DataArray( 
+            data=wst_ext, 
+            dims = ['t'],
+            coords = {'t':np.arange(life_h)})
+        xr_time['wdt'] = xr.DataArray( 
+            data=wdt_ext, 
+            dims = ['t'],
+            coords = {'t':np.arange(life_h)})
+
+        wake_loss_t = existing_wpp_power_curve_xr.wake_losses.interp(ws=xr_time.wst, wd=xr_time.wdt).values
+        wake_loss_t_ext = expand_to_lifetime(
+            wake_loss_t, life_h = life_h, weeks_per_season_per_year = weeks_per_season_per_year)
+
+        ws = existing_wpp_power_curve_xr.ws.values
+        pcw = existing_wpp_power_curve_xr.P_no_wake.values
+        
+        wind_t_ext_deg_no_wake = wpp_efficiency*get_wind_ts_degradation(
+            ws = ws, 
+            pc = pcw, 
+            ws_ts = wst_ext, 
+            yr = wind_deg_yr, 
+            wind_deg = wind_deg, 
+            life_h = life_h, 
+            share = share_WT_deg_types)
+        
+        wind_t_ext_deg = wake_loss_t_ext*wind_t_ext_deg_no_wake
+        
+        outputs['wind_t_ext_deg'] = wind_t_ext_deg
+
+
 # -----------------------------------------------------------------------
 # Auxiliar functions 
 # -----------------------------------------------------------------------        
